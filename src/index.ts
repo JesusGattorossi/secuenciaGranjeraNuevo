ff5e1ad5caa8b3980599abee8efb7e870505e194
JesusGattorossi <93558045+JesusGattorossi@users.noreply.github.com>
2021-11-05 08:49:11 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+//ejercicio 1 granjera
+console.log('avanzar');
+console.log('elimina1');
+console.log('avanzar');
+console.log('eliminar1');
+console.log('avanzar');
+console.log('eliminar1');
+console.log('avanzar');
+console.log('eliminar1');
+console.log('doblar a la izquierda');
+console.log('avanzar');
+console.log('eliminar1');
+console.log('avanzar');
+console.log('eliminar1');
+console.log('avanzar');
+console.log('eliminar1');
+console.log('avanzar');
+console.log('eliminar1');
+console.log('avanzar');
+console.log('eliminar1');
+
+//ejercicio 2 granjera
+console.log('avanzar');
+console.log('llenar1');
+console.log('avanzar');
+console.log('avanzar');
+console.log('avanzar');
+console.log('avanzar');
+console.log('llenar');
+console.log('avanzar');
+console.log('eliminar1');
+console.log('avanzar');
+console.log('eliminar1');
+console.log('avanzar');
+
+//ejercicio 3 granjera
+console.log('girar a la izquierda');
+console.log('avanzar');
+console.log('girar a la derecha');
+console.log('avanzar');
+console.log('avanzar');
+console.log('girar a la derecha');
+console.log('avanzar');
+console.log('eliminar1');
